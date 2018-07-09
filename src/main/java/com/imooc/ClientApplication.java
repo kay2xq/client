c8d24d35c6d87cd48083909897c764808cd6cf61
kay <kay2xq@163.com>
2018-07-09 14:26:30 +0800

    first commit

---
+++ src/main/java/com/imooc/ClientApplication.java
@@ -0,0 +1,15 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@EnableEurekaClient
+public class  ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
